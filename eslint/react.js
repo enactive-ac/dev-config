a663a2a80a086e9e994205bb0af25e8c82a17f91
Branden Visser <mrvisser@gmail.com>
2021-12-18 10:42:16 -0500

    fix(eslint/react): turn off import/default to support standard react import

--- eslint/react.js
+++ eslint/react.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: ['react-app', 'react-app/jest', `${__dirname}/index.js`],
   rules: {
     'jest/require-hook': 'off',
+    /** Turn off to support: `import React from 'react'` */
+    'import/default': 'off',
     'react/jsx-handler-names': ['error'],
     'react/jsx-sort-props': [
       'error',
